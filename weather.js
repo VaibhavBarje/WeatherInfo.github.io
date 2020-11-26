aca7df558bfd5dcd994cd36f219886caee6f8319
VaibhavBarje <vaibhavchandrakantbarje@gmail.com>
2020-11-26 19:55:37 +0530

    server_change

--- weather.js
+++ weather.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const fs = require("fs");
+const port = process.env.PORT || 3000;
 var requests = require("requests");
 
 const homeFile = fs.readFileSync("home.html", "utf-8");
@@ -36,4 +37,4 @@ const server = http.createServer((req, res) => {
 
 });
 
-server.listen(8000, "127.0.0.1");
+server.listen(port);
